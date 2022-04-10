0c2607e71b0f4d05ba043caff6da0ac29e8b8bab
Julian <julian.fm@live.com>
2022-04-10 11:44:21 -0300

    cart page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,11 @@ import styled from 'styled-components'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import Badge from '@mui/material/Badge';
+import {mobile} from "../responsive.js"
 
 const Container = styled.div`
   height: 60px;
+  ${mobile({ height: "50px"})}
 `;
 
 const Wrapper = styled.div`
@@ -13,6 +15,7 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  ${mobile({ padding: "10px 0px" })}
 `;
 
 const Left = styled.div`
@@ -23,7 +26,7 @@ align-items: center;
 `;
 
 const Language = styled.span`
-
+    ${mobile({ display: "none" })}
 `;
 const SearchContainer = styled.div`
   border: 0.5px solid lightgray;
@@ -35,6 +38,7 @@ const SearchContainer = styled.div`
 
 const Input = styled.input`
   border: none;
+  ${mobile({ width: "50px" })}
 `
 
 const Center = styled.div`
@@ -43,6 +47,7 @@ text-align: center;
 `
 const Logo = styled.h1`
 font-weight: bold;
+${mobile({ fontSize: "20px"})}
 `
 
 const Right = styled.div`
@@ -50,12 +55,14 @@ flex: 1;
 display: flex;
 align-items: center;
 justify-content: flex-end;
+${mobile({ flex: "2", justifyContent: "center" })}
 `
 
 const MenuItem = styled.div`
 font-size: 14px;
 cursor: pointer;
 margin-left: 25px;
+${mobile({ fontSize: "12px", marginLeft: "10px" })}
 `
 
 const Navbar = () => {
---
+++ src/pages/Login.jsx
@@ -0,0 +1,72 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+        rgba(0,0,0,0.5),
+        rgba(0,0,0,0.5)
+    ),
+    url("https://wallpaperaccess.com/full/4621939.jpg");
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+const Wrapper = styled.div`
+    padding: 20px;
+    width: 25%;
+    background-color: white;
+    justify-content: center;
+`
+
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 300;
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+`
+
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 10px 0;
+    padding: 10px;
+`
+const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+    margin-bottom: 10px;
+`
+const Link = styled.a`
+    margin: 5px 0px;
+    font-style: 12px;
+    text-decoration: underline;
+    cursor: pointer;
+`
+
+const Login = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <Title>sign in</Title>
+            <Form>
+                <Input placeholder="username"/>
+                <Input placeholder="password"/>
+                <Button>LOGIN</Button>
+                <Link>DO NOT REMEMBER?</Link>
+                <Link>CREATE A NEW ACCOUNT</Link>
+            </Form>
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default Login
