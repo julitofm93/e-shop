13fb8bc2792f0cf4e45558b71f0f55d59fe2e9f2
julitofm93 <julian.fm@live.com>
2022-04-10 12:52:10 -0300

    responsive design

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { mobile } from '../responsive'
 
 const Container = styled.div`
     width: 100vw;
@@ -18,6 +19,7 @@ const Wrapper = styled.div`
     width: 25%;
     background-color: white;
     justify-content: center;
+    ${mobile({ width: "75%"})}
 `
 
 const Title = styled.h1`
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -5,11 +5,14 @@ import Navbar from '../components/Navbar'
 import Newsletter from '../components/Newsletter'
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
+import { mobile } from '../responsive'
 
 const Container = styled.div``
 const Wrapper = styled.div`
     padding: 50px;
     display: flex;
+    ${mobile({ flexDirection: "column",
+                padding:"10px"})}
 `
 const ImgContainer = styled.div`
     flex: 1;
@@ -18,10 +21,12 @@ const Image = styled.img`
     width: 100%;
     height: 90vh;
     object-fit: contain;
+    ${mobile({ height: "40vh"})}
 `
 const InfoContainer = styled.div`
     flex: 1;
     padding: 0px 50px;
+    ${mobile({ padding: "10px"})}
 `
 const Title = styled.h1`
     font-weight: 200;
@@ -37,7 +42,8 @@ const FilterContainer = styled.div`
     width: 50%;
     margin: 30px 0px;
     display: flex;
-    justify-content: space-between;    
+    justify-content: space-between;
+    ${mobile({ width: "100%"})}    
 `
 const Filter = styled.div`
     display: flex;
@@ -66,6 +72,7 @@ const AddContainer = styled.div`
     align-items: center;
     width: 50%;
     justify-content: space-between;
+    ${mobile({ width: "100%"})}
 `
 const AmountContainer = styled.div`
     display: flex;
